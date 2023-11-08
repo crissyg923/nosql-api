89b15ee9809d84b95bded1e7913e2622a71963f4
Crystal Green <crissybrown1992@icloud.com>
2023-11-08 13:13:17 -0500

    completed thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -20,6 +20,25 @@ module.exports = {
         res.status(500).json(err);
     }
 },
+    async createThought(req,res) {
+        try{
+        const thought = await Thought.create(req.body);
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userId},
+            { $addToSet: { thoughts: thought._id }},
+            { new: true }
+        );
+        if (!user) {
+            return res.status(404).json({
+                message: 'No user found with that ID.'
+            })
+        }
+        res.status('Thought successfully created');
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
     async updateThought(req, res) {
         try {
             const thought = await Thought.findOneAndUpdate(
