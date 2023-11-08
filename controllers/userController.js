9d1b0cc3810b54fb36d08fd5a2be36f21b0fb22e
Crystal Green <crissybrown1992@icloud.com>
2023-11-08 13:38:16 -0500

    completed user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,4 +78,22 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async removeFriend(req,res) {
+        // console.log('You are deleting a friend');
+        
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params._userId},
+                { $pull: { friends: { friendId: req.params.friendId } } },
+                { runValidators: true, new: true}
+                );
+                
+                if (!user) {
+                    return res.status(404).json({ message: 'No user found with that ID.'});
+                }
+                res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
