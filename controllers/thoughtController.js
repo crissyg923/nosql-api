ed440684c22898e78400149ea739fe25b8e49681
Crystal Green <crissybrown1992@icloud.com>
2023-11-08 12:46:07 -0500

    added user and thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,24 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req,res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+            if (!thought) {
+                return  res.status(404).json({message: 'No user with that ID!'})
+            }
+            res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,45 @@
 const { User, Thought } = require('../models');
 
+// get all users, user by id, post new user
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req,res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId})
+            .populate({ path: 'thoughts', select: '-__v'})
+            .populate({ path: 'friends', select: '-__v'});
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID'})
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req,res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: request.params.userId});
+            if (!user) {
+                return res.status(404).json({ message: 'User not found.'})
+            }
+            res.json({ message: 'User successfully deleted'});
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
